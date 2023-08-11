7b7d1ccc0df813cf08a0b9e212b22ea7707aa451
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-08-11 15:20:20 +0500

    wiki handler add

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,15 @@
 from aiogram import types
-
+import wikipedia
 from loader import dp
+
+wikipedia.set_lang('uz')
 
 
 # Echo bot
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
